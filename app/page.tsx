5a8f80e8d5b6d8718ec5f7d793288bbf1580e2ec
Trieu Pham <trieu@trieupham.com>
2024-11-20 09:12:40 -0600

    add storage backend

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import { Authenticator } from '@aws-amplify/ui-react';
+import FileUploader from './fileuploader';
 import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
@@ -14,44 +15,16 @@ Amplify.configure(outputs);
 const client = generateClient<Schema>();
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
-
   return (
     <Authenticator>
       {({ signOut, user }) => (
-        <main>
-          <h1>{user?.signInDetails?.loginId}'s todos</h1>
-          <button onClick={createTodo}>+ new</button>
-          <ul>
-            {todos.map((todo) => (
-              <li key={todo.id}>{todo.content}</li>
-            ))}
-          </ul>
-          <div>
-            🥳 App successfully hosted. Try creating a new todo.
-            <br />
-            <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-              Review next steps of this tutorial.
-            </a>
-          </div>
-          <button onClick={signOut}>Sign out</button>
-        </main>
+        <FileUploader
+          acceptedFileTypes={['image/*']}
+          path={({ identityId }) => `protected/${identityId}/`}
+          autoUpload={false}
+          maxFileCount={1}
+          isResumable 
+        />
       )}
     </Authenticator>
   );
